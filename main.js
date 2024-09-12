2c967acfd063ecd542e7435ef7cf304758e008d7
tjwls11 <09sj03@naver.com>
2024-09-12 15:36:49 +0900

    first

---
+++ main.js
@@ -0,0 +1,45 @@
+document.getElementById('time').innerHTML = new Date().toLocaleDateString()
+
+function pink(){
+  document.body.style.backgroundColor = 'pink'
+}
+
+function Lightblue(){
+  document.body.style.backgroundColor = 'Lightblue'
+}
+
+function Yellow(){
+  document.body.style.backgroundColor = 'Yellow'
+}
+
+function Gray(){
+  document.body.style.backgroundColor = 'Gray'
+}
+
+function Reset(){
+  document.body.style.backgroundColor = 'linen'
+}
+
+function showhtml() {
+  document.getElementById('fig').src = 'img/html5.jpg'
+  document.getElementById('desc').innerHTML =
+    '<b>HTML</b>은 구조적 웹문서를 작성하는데 사용하는 마크업 언어이다.'
+}
+
+function showcss() {
+  document.getElementById('fig').src = 'img/css3.jpg'
+  document.getElementById('desc').innerHTML =
+    '<b>CSS</b>는 HTML 문서의 스타일을 지정하기 위한 언어이다.'
+}
+
+function showjs() {
+  document.getElementById('fig').src = 'img/javascript.jpg'
+  document.getElementById('desc').innerHTML =
+  '<b>Javascript</b>는 웹을 위한 프로그래밍 언어이며 웹문서에서 동작을 실행할 수 있게 한다. '
+}
+
+function hide() {
+  document.getElementById('fig').src = ''
+  document.getElementById('desc').innerHTML =''
+}
+
